3d0a18b4cf1b7d9fcbeb2130e9860471fc33f7ae
Matthew Olson <Olson@Matthews-MacBook-Air-2.local>
2016-04-06 20:21:16 -0400

    Finalized enroll page and wait times

--- enroll.py
+++ enroll.py
@@ -3,6 +3,10 @@ import time as timeModule
 from datetime import datetime,date,timedelta,time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 if __name__ == '__main__':
 	username = raw_input("Username: ")
@@ -17,4 +21,25 @@ if __name__ == '__main__':
 	elem2.send_keys(password)
 	driver.find_element_by_name('Submit').click()
 
+	wait = WebDriverWait(driver, 10)
+	element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT,'Registration')))
+	driver.implicitly_wait(2)
 	driver.find_element_by_link_text("Registration").click()
+
+	wait = WebDriverWait(driver, 10)
+	element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT,'bookbag')))
+	driver.implicitly_wait(2)
+	driver.find_element_by_link_text("bookbag").click()
+
+	waitTime = datetime(2016, 4, 7, 7, 0, 0, 0) - datetime.today()
+	if (waitTime.total_seconds()>0):
+		print 'Waiting to check in for '+  str(waitTime)
+		timeModule.sleep(waitTime.total_seconds())
+
+	wait = WebDriverWait(driver, 10)
+	element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT,'Go To Enroll Page')))
+	driver.find_element_by_link_text("Go To Enroll Page").click()
+
+	wait = WebDriverWait(driver, 10)
+	element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT,'Go To Enroll Page')))
+	driver.find_element_by_link_text("Finish Enrolling").click()
